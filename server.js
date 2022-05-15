28d560560ccff203966404189fdeb5e1e7ae4b42
nahzryuab <bauyrzhan.bilal@gmail.com>
2022-05-15 16:52:43 +0600

    sosat'

--- server.js
+++ server.js
@@ -1,3 +1,77 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const ejs=require('ejs')
+// const methodOverride = require('method-override')
+// const app = express();
+// //const port = 3000
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+// app.set('view engine', 'ejs')
+// app.use(express.static('public'))
+//
+// app.use(methodOverride('_method'))
+//
+// const UserRoute = require('./routes/index')
+// app.use('/user',UserRoute)
+//
+// const dbConfig = require('./config/database.config.js');
+// const mongoose = require('mongoose');
+// const {router} = require("express/lib/application");
+//
+// mongoose.Promise = global.Promise;
+// mongoose.connect("mongodb+srv://nahzryuab:stayugly241042@cluster0.r3edx.mongodb.net/Shop?retryWrites=true&w=majority", {
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log("Database Connected Successfully!!");
+// }).catch(err => {
+//     console.log('Could not connect to the database', err);
+//     process.exit();
+// });
+//
+//
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+//
+// /*app.post('/', (req, res) => {
+//     create(req,res)
+// });*/
+//
+// /*app.get('/', (req, res) => {
+//     findAll(req, res)
+// });*/
+//
+// app.get('/find', (req, res) => {
+//     res.render('find');
+// });
+// /*app.get('/findbyemail', (req, res) => {
+//     findOne(req,res)
+// });*/
+//
+// app.get('/update', (req, res) => {
+//     res.render('update');
+// });
+//
+// app.get('/delete', (req, res) => {
+//     res.render('delete');
+// });
+//
+//
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//     port = 3000;
+// }
+//
+// app.listen(port, () => {
+//     console.log(`Server is listening on port http://localhost:${port}`);
+// });
+
+
+
+
+
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
@@ -31,7 +105,7 @@ router
             password: req.body.password
         })
         newUser.save();
-        res.redirect('/');
+        res.redirect('/index');
     })
 
 module.exports = router;
