6aec1fe87f22c2d455527018dc774ee8e378991d
nahzryuab <bauyrzhan.bilal@gmail.com>
2022-05-17 14:50:10 +0600

    mongo db

--- routes/reg.js
+++ routes/reg.js
@@ -1,8 +1,13 @@
 const express = require("express");
-
 const router = express.Router();
+const app = express();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+const Post = require('../model/user')
+const UserAddController = require('../controllers/UserAddController')
 router
     .route("/")
-    .get((req, res) => res.render(__dirname+"/reg.ejs"))
-    .post(controller.addUser);
+    .get((req, res) => res.render('/reg.ejs'))
+    .post(UserAddController.addUser)
+
 module.exports = router;
